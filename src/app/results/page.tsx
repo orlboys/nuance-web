882147d5ebb9e54ac098952bf458014a17c540a2
Oscar B <oscar.boys2@outlook.com>
2025-05-31 16:21:26 +1000

    fix: making linter happy, removing unneccesary imports

--- src/app/results/page.tsx
+++ src/app/results/page.tsx
@@ -1,8 +1,6 @@
 "use client";
 
 import * as React from "react";
-import { useTheme } from "@mui/material/styles";
-import { Stack } from "@mui/material";
 import { Meter } from "./components/ui/Gauge";
 
 export default function Results() {
