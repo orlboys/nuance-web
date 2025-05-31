c57d2b59dc78931ffb1e064dea2532f851992fac
Oscar B <oscar.boys2@outlook.com>
2025-05-31 16:17:05 +1000

    feat: add Meter component for displaying gauge and integrate into Results page

---
+++ src/app/results/components/ui/Gauge.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import * as React from "react";
+import { useTheme } from "@mui/material/styles";
+import { Gauge, gaugeClasses } from "@mui/x-charts/Gauge";
+
+export interface MeterProps {
+  value: number;
+  width?: number;
+  height?: number;
+  ariaLabel?: string;
+}
+
+export const Meter = ({
+  value,
+  width = 600,
+  height = 600,
+  ariaLabel = "Bias Meter",
+}: MeterProps) => {
+  const theme = useTheme();
+
+  return (
+    <Gauge
+      value={Math.ceil(value)}
+      width={width}
+      height={height}
+      startAngle={-90}
+      endAngle={90}
+      aria-label={ariaLabel}
+      sx={{
+        [`& .${gaugeClasses.valueText}`]: {
+          fontSize: 50,
+          fill: theme.palette.text.primary,
+        },
+        [`& .${gaugeClasses.valueArc}`]: {
+          fill: theme.palette.primary.main,
+        },
+        [`& .${gaugeClasses.referenceArc}`]: {
+          fill: theme.palette.text.disabled,
+        },
+      }}
+    />
+  );
+};
