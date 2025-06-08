ee524586952f4fef3c8b2c4331fafaec7f8fd3a1
Oscar B <oscar.boys2@outlook.com>
2025-06-08 14:47:02 +1000

    chore: removed unused imports

--- src/app/signup/components/LogInCard.tsx
+++ src/app/signup/components/LogInCard.tsx
@@ -3,8 +3,6 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
-import Checkbox from "@mui/material/Checkbox";
-import FormControlLabel from "@mui/material/FormControlLabel";
 import Divider from "@mui/material/Divider";
 import FormLabel from "@mui/material/FormLabel";
 import FormControl from "@mui/material/FormControl";
